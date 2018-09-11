6ffacee7d99631ada4a494210ba5996771259a56
Innocent <31923567+angwandi@users.noreply.github.com>
2018-09-11 19:32:40 +0100

    Add more activities

---
+++ app/src/main/java/app/a2ms/flickrbrowser/SearchActivity.kt
@@ -0,0 +1,16 @@
+package app.a2ms.flickrbrowser
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_search.*
+
+class SearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+}
