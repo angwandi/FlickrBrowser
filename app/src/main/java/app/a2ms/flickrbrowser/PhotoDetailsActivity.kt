57f86353da883fa47de67a8b78b780b50f100311
Innocent <31923567+angwandi@users.noreply.github.com>
2018-09-12 22:06:35 +0100

    Modify PhotoDetailActivity to receive data

--- app/src/main/java/app/a2ms/flickrbrowser/PhotoDetailsActivity.kt
+++ app/src/main/java/app/a2ms/flickrbrowser/PhotoDetailsActivity.kt
@@ -1,6 +1,8 @@
 package app.a2ms.flickrbrowser
 
 import android.os.Bundle
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.content_photo_details.*
 
 class PhotoDetailsActivity : BasicActivity() {
 
@@ -8,5 +10,16 @@ class PhotoDetailsActivity : BasicActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_details)
         activateToolbar(true)
+
+        val photo = intent.getSerializableExtra(PHOTO_TRANSFER) as Photo
+        photo_title.text = photo.title
+        photo_tags.text = photo.tags
+        photo_author.text = photo.author
+
+        Picasso.with(this).load(photo.link)
+                .error(R.drawable.placeholder)
+                .placeholder(R.drawable.placeholder)
+                .into(photo_image)
     }
+
 }
